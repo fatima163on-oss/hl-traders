52506745e7a82a4369b7f7cbc8aafbf24861112c
fatima163on-oss <fatima163on@gmail.com>
2026-06-14 11:33:48 +0300

    Update hl.js

--- pages/api/hl.js
+++ pages/api/hl.js
@@ -1,3 +1,5 @@
+const fetch = (...args) => import('node-fetch').then(({default: f}) => f(...args));
+
 export default async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
